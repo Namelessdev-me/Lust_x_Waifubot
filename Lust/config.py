eada8ac71339c9555448e234ef2690f1a73fe223
ＴｅａｍＳｈａｄｏｗｓ <alinwalker252@gmail.com>
2026-05-26 16:24:25 +0530

    Update config.py

--- Lust/config.py
+++ Lust/config.py
@@ -1,13 +1,13 @@
 import os
 OWNER_ID = 8779269421
-GROUP_ID = "-1002486274374"
+GROUP_ID = -1002486274374
 TOKEN = "8985107066:AAFgACYTkQ-RmSU6WCl4oZDT9ParnvcjVOY"
 api_id = 35009179
 api_hash = "a76bbe9b7a26e7ad2af351b07dca34e2"
 PHOTO_URL = ["https://files.catbox.moe/82wsna.mp4"]
 SUPPORT_CHAT = "+hye3u0fBPO1lNjM1"
 UPDATE_CHAT = "lustXwaifu"
-BOT_USERNAME = "AniLust_Bot"
+BOT_USERNAME = "LustXwaifu_bot"
 CHARA_CHANNEL_ID = -1003852106890
 MONGO_URL = "mongodb+srv://lustbot:lustbot@lustbot.dhfa9ty.mongodb.net/?appName=lustbot"
 DATABASE_NAME = "LustXDatabase"
