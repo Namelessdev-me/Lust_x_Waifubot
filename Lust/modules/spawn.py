dd6d163aad3af3e4b1632a8d6f5deb8e549e1766
raysidead0 <eliterolex20099@gmail.com>
2026-05-29 16:05:40 +0530

    Update spawn.py

--- Lust/modules/spawn.py
+++ Lust/modules/spawn.py
@@ -84,7 +84,7 @@ async def spawn_character(chat_id):
             "🐦‍🔥 Exotic":  0.0,
             "🎐 Devine":    0.1,
             "💦 Wet":    0.3,
-            "🎥 Animation": 50.0
+            "🎥 Animation": 0.1
         }
 
 
