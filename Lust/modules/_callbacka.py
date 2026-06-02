76acee2be0cb48ab32a1cc660b63ea73a12507f2
⎝⎝Nⱥ𝓶lͥesͣsͫ  𝘊𝙤𝙙𝙚𝙧 ⎝⎝ <alinwalker252@gmail.com>
2026-06-02 11:28:22 +0530

    Update _callbacka.py

--- Lust/modules/_callbacka.py
+++ Lust/modules/_callbacka.py
@@ -1,14 +1,11 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CallbackContext, CallbackQueryHandler, CommandHandler
 from Lust import application
-#from .cmode import cmode_callback
 from Lust.utils.button import button_click as bc
-#from .myslaves import myslaves_callback as hc
 from .info import check
-#from .sgift import confirm_gift, cancel_gift
 from .trade import confirm_trade, cancel_trade
-from .rps import rps_button 
-#from .start import button
+from .rps import rps_button
+from .sgift import gift_callback
 from .block import block_cbq_ptb
 
 @block_cbq_ptb
@@ -21,7 +18,9 @@ async def cbq(update: Update, context: CallbackContext):
         await confirm_trade(update, context)
     elif data.startswith('cancel_trade'):
         await cancel_trade(update, context)
+    elif data.startswith('con_gift') or data.startswith('can_gift'):
+        await gift_callback(update, context)
     elif data in ('rock', 'paper', 'scissors', 'play_again'):
         await rps_button(update, context)
-        
+
 application.add_handler(CallbackQueryHandler(cbq, pattern='.*'))
